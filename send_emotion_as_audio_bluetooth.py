dc0b7b33452d26c472bcde2099b1d60f30bbf378
MartyRath <martydavidger@gmail.com>
2023-12-27 16:22:40 +0000

    Issues with playsound. Converting emotion audio files from m4a to mp3 using ffmeg

--- send_emotion_as_audio_bluetooth.py
+++ send_emotion_as_audio_bluetooth.py
@@ -1,25 +1,25 @@
 import random
 import time
-import playsound
+from playsound import playsound
 import os
 
 emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
+# Dictionary mapping emotions to audio file names or paths
+emotion_audio = {
+    'Angry': os.path.join('audio', 'angry.mp3'),
+    'Disgust': os.path.join('audio', 'disgust.mp3'),
+    'Fear': os.path.join('audio', 'fear.mp3'),
+    'Happy': os.path.join('audio', 'happy.mp3'),
+    'Neutral': os.path.join('audio', 'sad.mp3'),
+    'Sad': os.path.join('audio', 'sad.mp3'),
+    'Surprise': os.path.join('audio', 'surprise.mp3')
+}
+
 while True:
     random_emotion = random.choice(emotions)
     print(random_emotion)
-    time.sleep(5)  # Wait for 5 seconds before generating the next emotion
-
-# Dictionary mapping emotions to audio file names or paths
-emotion_audio = {
-    'Angry': os.path.join('audio', 'angry.m4a'),
-    'Disgust': os.path.join('audio', 'disgust.m4a'),
-    'Fear': os.path.join('audio', 'fear.m4a'),
-    'Happy': os.path.join('audio', 'happy.m4a'),
-    'Neutral': os.path.join('audio', 'sad.m4a'),
-    'Sad': os.path.join('audio', 'sad.m4a'),
-    'Surprise': os.path.join('audio', 'surprise.m4a')
-}
-
-audio_file = emotion_audio[random_emotion]
-playsound(audio_file)
+    
+    audio_file = emotion_audio[random_emotion]
+    playsound(audio_file)
+    time.sleep(5)
