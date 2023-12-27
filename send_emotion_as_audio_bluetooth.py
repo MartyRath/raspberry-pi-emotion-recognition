86fb73659cca9007904c8d1d7b7c9d17ebf5d207
MartyRath <martydavidger@gmail.com>
2023-12-27 16:40:16 +0000

    Emotion audio plays via default speaker using pygame

--- send_emotion_as_audio_bluetooth.py
+++ send_emotion_as_audio_bluetooth.py
@@ -1,12 +1,15 @@
 import random
 import time
-from playsound import playsound
+import pygame
 import os
 
-emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+# Initialise pygame
+pygame.init()
 
-# Dictionary mapping emotions to audio file names or paths
-emotion_audio = {
+emotion_text = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+# Dictionary mapping emotions to audio files
+emotion_text_to_audio = {
     'Angry': os.path.join('audio', 'angry.mp3'),
     'Disgust': os.path.join('audio', 'disgust.mp3'),
     'Fear': os.path.join('audio', 'fear.mp3'),
@@ -17,9 +20,19 @@ emotion_audio = {
 }
 
 while True:
-    random_emotion = random.choice(emotions)
+    random_emotion = random.choice(emotion_text)
     print(random_emotion)
+    emotion_audio = emotion_text_to_audio[random_emotion]
+
+    # Load the audio file
+    audio = pygame.mixer.Sound(emotion_audio)
     
-    audio_file = emotion_audio[random_emotion]
-    playsound(audio_file)
+    # Play the audio file
+    audio.play()
+
+    # Checks if audio is still playing to allow it to play in full
+    while pygame.mixer.get_busy():
+        # Checks if audio still playing every 10 frames
+        pygame.time.Clock().tick(10)
+
     time.sleep(5)
