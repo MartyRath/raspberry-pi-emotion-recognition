940cd324fdab6739da6c57cd0285e91dd9bb3830
MartyRath <martydavidger@gmail.com>
2023-12-29 12:30:56 +0000

    store_captured_images now uploads images to Firebase if they don't exist

--- firebase/store_captured_images_FB.py
+++ firebase/store_captured_images_FB.py
@@ -1,6 +1,6 @@
 # Uploads captured_images to firebase.
 import firebase_admin
-from firebase_admin import credentials, firestore, storage, db
+from firebase_admin import credentials, storage
 import os
 
 cred=credentials.Certificate('./serviceAccountKey.json')
@@ -11,8 +11,18 @@ firebase_admin.initialize_app(cred, {
 
 bucket = storage.bucket()
 
-captured_images = '../captured_images'
+captured_images_path = '../captured_images'
 
-# Function returns list of current image filenames 
-image_filenames = os.listdir(captured_images)
-print(image_filenames)
+# Returns list of current image filenames 
+image_filenames = os.listdir(captured_images_path)
+
+# Loop through image filenames
+for filename in image_filenames:
+    # Check if the image has already been uploaded by comparing with Firebase storage
+    blob = bucket.blob(filename)
+    if not blob.exists():
+        # Upload the image to Firebase Storage
+        blob.upload_from_filename(os.path.join(captured_images_path, filename))
+        print(f"Uploaded {filename} to Firebase Storage")
+
+print ("Firebase up-to-date")
