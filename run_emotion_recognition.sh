238f953235bfa8f5d60ffff12dd700318bea617a
MartyRath <martydavidger@gmail.com>
2023-12-31 16:41:08 +0000

    Redirect output and errors to log file to debug

--- run_emotion_recognition.sh
+++ run_emotion_recognition.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cd /home/marty/assignment/raspberry-pi-emotion-recognition
+# Redirect output to a log file to debug
+exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
+
+# Navigate to the directory
+cd /home/marty/assignment/raspberry-pi-emotion-recognition || { echo "Failed to change directory. Exiting."; exit 1; }
 
 # Activate virtual environment
-source /home/marty/assignment/raspberry-pi-emotion-recognition/virtualenv/bin/activate
+source /home/marty/assignment/raspberry-pi-emotion-recognition/virtualenv/bin/activate || { echo "Failed to activate virtual environment. Exiting."; exit 1; }
 
 # Run emotion detection
 python3 01_emotion_recognition_all_features.py
