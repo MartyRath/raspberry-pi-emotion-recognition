3219b30cd52eeafcf16d70b740c321d5a3b32620
MartyRath <martydavidger@gmail.com>
2024-01-01 18:08:10 +0000

    Connects bluetooth device before starting emotion recognition

--- run_emotion_recognition.sh
+++ run_emotion_recognition.sh
@@ -2,9 +2,15 @@
 # This navigates to emotion recogntion project directory, starts virtual environment
 # and then starts emotion recognition.
 
-# Clear contents of the debug.log file
-> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
-exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
+DEVICE_NAME="Jabra Elite Active 75t"
+
+# While bluetoothctl info does not contain "Connected: yes", do...
+while ! bluetoothctl info | grep -q "Connected: yes"; do
+    # Attempt to connect
+    bluetoothctl connect "$DEVICE_NAME"
+    sleep 2
+done
+echo "Bluetooth connected"
 
 cd /home/marty/assignment/raspberry-pi-emotion-recognition
 
