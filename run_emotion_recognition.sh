62dba6cfe16cd7b9400f860356e6c04bbc65d805
MartyRath <martydavidger@gmail.com>
2024-01-01 18:44:20 +0000

    debug wasn't updating as had sheband with "sh" instead of "bash"

--- run_emotion_recognition.sh
+++ run_emotion_recognition.sh
@@ -1,16 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 # This navigates to emotion recogntion project directory, starts virtual environment
 # and then starts emotion recognition.
 
-DEVICE_NAME="Jabra Elite Active 75t"
+# Redirect output to a log file to debug
+exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
+
+device_name="Jabra Elite Active 75t"
 
 # While bluetoothctl info does not contain "Connected: yes", do...
 while ! bluetoothctl info | grep -q "Connected: yes"; do
     # Attempt to connect
-    bluetoothctl connect "$DEVICE_NAME"
+    bluetoothctl connect "$device_name"
     sleep 2
 done
 echo "Bluetooth connected"
+
+# gets bluetooth audio sink index from list
+bluetooth_index=$(pactl list short sinks | grep "bluez" | awk '{print $1}')
+# Sets default audio sink by index from list
+pactl set-default-sink $bluetooth_index
 
 cd /home/marty/assignment/raspberry-pi-emotion-recognition
 
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Redirect output to a log file to debug
+exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
+
+# Test command to generate output
+echo "This is a test message"
