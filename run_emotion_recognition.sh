24a82a8b0110b8b12167311c73580dcaa22b570f
MartyRath <martydavidger@gmail.com>
2024-01-01 19:29:32 +0000

    pulseaudio --start solved it

--- run_emotion_recognition.sh
+++ run_emotion_recognition.sh
@@ -5,10 +5,10 @@
 # Clear log file
 > /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
 
-
 # Redirect output to a log file to debug
 exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
 
+pulseaudio --start
 device_name="Jabra Elite Active 75t"
 
 # While bluetoothctl info does not contain "Connected: yes", do...
@@ -19,13 +19,7 @@ while ! bluetoothctl info | grep -q "Connected: yes"; do
 done
 echo "Bluetooth connected"
 
-# gets bluetooth audio sink index from list
-bluetooth_index=$(pactl list short sinks | grep "bluez" | awk '{print $1}')
-# Sets default audio sink by index from list
-pactl set-default-sink $bluetooth_index
-
-# Redirect output to a log file to debug
-exec &>> /home/marty/assignment/raspberry-pi-emotion-recognition/debug.log
+echo "starting emotion file"
 
 cd /home/marty/assignment/raspberry-pi-emotion-recognition
 
